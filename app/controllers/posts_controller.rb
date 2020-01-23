9213a90d1b4c2f934a4c8f84de3ba2c16eb8e5a4
Gilblasse <gilblasse@gmail.com>
2020-01-23 17:37:28 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,17 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    
+    if @post.update_attributes(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
-    redirect_to post_path(@post)
   end
+
+
+
 
   private
 
