af666af129a78c2bc877a018bb9cdd9e92a91085
Ivana <ivana_pile@yahoo.com>
2020-02-04 11:46:02 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, uniqueness: true
+#The name cannot be blank
+#The e-mail is unique
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+	validates :content, length: { minimum: 100 }
+	validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    #The title cannot be blank
+    #The category is either "Fiction" or "Non-Fiction"
+    #The content is at least 100 characters long.
 end
