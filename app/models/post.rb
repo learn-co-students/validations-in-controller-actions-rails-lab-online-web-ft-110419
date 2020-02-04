f2b568a7e5ed9297fb0a2b2af36f8be4b36def99
nkizito92 <nkizito57@gmail.com>
2020-02-03 22:07:32 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} This is neither a Fiction or Non-Fiction category"}
+    validates :content, length: { minimum: 100}
 end
